5d06e8b2fcff8ebd994cf8d28beb8c2a6aaba5a0
AUGUSTO GOMEZ <agr120@hotmail.com>
2022-06-27 22:32:05 -0600

    MOD app main

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SellShop } from "./components/SellShop";
-import { ProductsShop } from "./components/ProductsShop";
+import React from 'react';
+import { SellShop } from './components/SellShop';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/sell" element={<SellShop />} />
-
-          <Route path="/prod" element={<ProductsShop />} />
-        </Routes>
-      </BrowserRouter>
+    <SellShop />
     </>
   );
 }
